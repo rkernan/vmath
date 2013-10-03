45cf822d58f5e08ad20327233b6956a66c2c71d5
Robert Kernan <rkernan@gmail.com>
2013-10-03 00:40:45 -0400

    Created a Visual Studio Project. Does not build.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
 	for (int i = 0; i < argc; ++i) {
 		std::string arg(argv[i]);
-		if (arg == std::string("-test") || arg == std::string("-t")) {
+		if (arg.compare(std::string("-test")) == 0 || arg.compare(std::string("-t")) == 0) {
 			static_assert(sizeof(math::vector2) == sizeof(float[2]), "sizeof(vector2) != sizeof(float[2])");
 			static_assert(sizeof(math::vector3) == sizeof(float[3]), "sizeof(vector3) != sizeof(float[3])");
 			static_assert(sizeof(math::vector4) == sizeof(float[4]), "sizeof(vector4) != sizeof(float[4])");
