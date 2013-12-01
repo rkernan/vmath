231e4d689eab32313051e9d7a0006f5b4900d2de
Robert Kernan <rkernan@gmail.com>
2013-12-01 02:03:52 -0500

    Removed doesn't force cmake anymore.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ build_dir=build
 if [[ ! -e $build_dir ]]; then
 	mkdir $build_dir
 	cd $build_dir
-	cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+	cmake ..
 elif [[ ! -d $build_dir ]]; then
 	echo "Build directory ($build_dir) exists, but it is not a directory."
 else
