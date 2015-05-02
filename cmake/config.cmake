9248cd69fcfdabdfe8798d88e5e89b0059a06cbe
Robert Kernan <rkernan@gmail.com>
2015-05-02 14:05:30 -0400

    Update compiler in travis build.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,6 +1,2 @@
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-	add_definitions("-std=c++11")
-endif()
-
 option(MATH_BUILD_DOC "Built HTML documentation." ON)
 option(MATH_BUILD_TESTS "Build tests." ON)
