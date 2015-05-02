98cc5ae84aad6e1c5f8da51a36332c28ccac87b6
Robert Kernan <rkernan@gmail.com>
2015-05-02 15:50:47 -0400

    Revert to cmake 2.8 for now.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,2 +1,6 @@
+if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	add_definitions("-std=c++11")
+endif()
+
 option(MATH_BUILD_DOC "Built HTML documentation." ON)
 option(MATH_BUILD_TESTS "Build tests." ON)
