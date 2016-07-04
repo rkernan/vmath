a89424dd78f8a185cdf6f637a8ff996c03fa8f65
Robert Kernan <rkernan@gmail.com>
2016-07-04 17:08:44 -0400

    Removed configuration.
    Setting up doc step to automatically skip doc if doxygen is not found.

--- cmake/config.cmake
+++
@@ -1,6 +0,0 @@
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-	add_definitions("-std=c++11")
-endif()
-
-option(VMATH_BUILD_DOC "Built HTML documentation." ON)
-option(VMATH_BUILD_TESTS "Build tests." ON)
