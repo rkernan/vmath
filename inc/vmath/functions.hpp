4d9e87598c7bc7708bc2b5be480d8f02e6db6387
Robert Kernan <rkernan@gmail.com>
2017-05-13 16:43:48 -0400

    Refactored functions
    Issue #12, Issue #14

--- inc/vmath/functions.hpp
+++ inc/vmath/functions.hpp
@@ -8,54 +8,37 @@ namespace vmath {
 
 const long double PI = std::acos(-1.0L);
 
-/*!
- * \brief Determine if two float are equivalent within a given tolerance
- * \param[in] a Float to compare
- * \param[in] b Float to compare
- * \param[in] ulp The desired floating point precision in ULPs (units in the last place), default 2
- * \return True if they are equivalent, otherwise false
- */
 template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
-bool equals(const T a, const T b, const int ulp=2);
+bool equals(const T& a, const T& b) {
+	return std::fabs(a - b) < std::numeric_limits<T>::epsilon() * std::fabs(a + b) * std::fmax(a, b) ||
+	       // unless the result is sub-normal
+	       std::fabs(a - b) < (std::numeric_limits<T>::min)(); // NOTE: parens around min() to prevent collision with windows.h min macro
+}
 
-/*!
- * \brief Convert degrees to radians
- * \param[in] degrees Degrees to convert
- * \return The given degrees in radians
- */
 template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
-T radians(const T degrees);
+T radians(const T& degrees) {
+	static const T PI_OVER_180 = static_cast<T>(vmath::PI / 180.0L);
+	return degrees * PI_OVER_180;
+}
 
-/*!
- * \brief Convert radians to degrees
- * \param[in] radians Radians to convert
- * \return The given radians in degrees
- */
 template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
-T degrees(const T radians);
-
-/*!
- * \brief Clamp into a range
- * \param[in] s Scalar to clamp
- * \param[in] lo Lowest number included in the range
- * \param[in] hi Highest number included in the range
- * \return Clamped scalar
- */
-template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
-T clamp(const T s, const T lo, const T hi);
-
-/*!
- * \brief Linearly interpolate scalars
- * \param[in] s Start value
- * \param[in] e End value
- * \param[in] t Amount to interpolate by
- * \return Interpolated scalar
- */
-template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
-T lerp(const T s, const T e, const T t);
+T degrees(const T& radians) {
+	static const T PI_UNDER_180 = static_cast<T>(180.0L / vmath::PI);
+	return radians * PI_UNDER_180;
 
 }
 
-#include <vmath/functions.inl>
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+T lerp(const T& s, const T& e, const T& t) {
+	return s + ((e - s) * t);
+}
+
+// TODO C++17 is adding std::clamp!
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+T clamp(const T& s, const T& lo, const T& hi) {
+	return (s <= lo) ? lo : ((s >= hi) ? hi : s);
+}
+
+} // namespace vmath
 
 #endif
--- test/functions.cpp
+++ test/functions.cpp
@@ -1,39 +1,33 @@
-#include <boost/test/test_case_template.hpp>
-#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE functions
+#include "boost_test_config.hpp"
 
 #include <vmath/functions.hpp>
-#include "type_lists.hpp"
 
-BOOST_AUTO_TEST_SUITE(functions)
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(pi, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(pi, T, floating_point_types) {
 	BOOST_CHECK_CLOSE(static_cast<T>(vmath::PI), static_cast<T>(3.14159265358979323846), 1e-4f);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(equals, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(equals, T, floating_point_types) {
 	BOOST_CHECK(vmath::equals(static_cast<T>(1.2), static_cast<T>(1.2)));
-	BOOST_CHECK(!vmath::equals(static_cast<T>(1.2), static_cast<T>(1.3)));
+	BOOST_CHECK(!vmath::equals(static_cast<T>(1.2), static_cast<T>(1.200001)));
+	BOOST_CHECK(!vmath::equals(static_cast<T>(1.2), static_cast<T>(1.199999)));
+	BOOST_CHECK(vmath::equals(static_cast<T>(-18.2), static_cast<T>(-18.2)));
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(equals_specify_ulp, T, float_types) {
-	BOOST_CHECK(vmath::equals(static_cast<T>(1.2), static_cast<T>(1.2), 3));
-	BOOST_CHECK(!vmath::equals(static_cast<T>(1.2), static_cast<T>(1.3), 3));
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(degrees_to_radians, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(degrees_to_radians, T, floating_point_types) {
 	BOOST_CHECK_SMALL(vmath::radians(static_cast<T>(0.0)), static_cast<T>(1e-7));
 	BOOST_CHECK_CLOSE(vmath::radians(static_cast<T>(180.0)), static_cast<T>(vmath::PI), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::radians(static_cast<T>(23.5)), static_cast<T>(0.410152374), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::radians(static_cast<T>(985.0)), static_cast<T>(17.1914921), 1e-4f);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(radians_to_degrees, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(radians_to_degrees, T, floating_point_types) {
 	BOOST_CHECK_SMALL(vmath::degrees(static_cast<T>(0.0)), static_cast<T>(1e-7));
 	BOOST_CHECK_CLOSE(vmath::degrees(static_cast<T>(vmath::PI)), static_cast<T>(180.0), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::degrees(static_cast<T>(0.345)), static_cast<T>(19.76704393), 1e-4f);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(clamp, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(clamp, T, floating_point_types) {
 	BOOST_CHECK_CLOSE(vmath::clamp(static_cast<T>(1.2), static_cast<T>(1.5), static_cast<T>(2.0)), static_cast<T>(1.5), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::clamp(static_cast<T>(1.5), static_cast<T>(1.5), static_cast<T>(2.0)), static_cast<T>(1.5), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::clamp(static_cast<T>(1.75), static_cast<T>(1.5), static_cast<T>(2.0)), static_cast<T>(1.75), 1e-4f);
@@ -41,11 +35,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(clamp, T, float_types) {
 	BOOST_CHECK_CLOSE(vmath::clamp(static_cast<T>(2.2), static_cast<T>(1.5), static_cast<T>(2.0)), static_cast<T>(2.0), 1e-4f);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(lerp, T, float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(lerp, T, floating_point_types) {
 	BOOST_CHECK_CLOSE(vmath::lerp(static_cast<T>(1.0), static_cast<T>(2.0), static_cast<T>(0.0)), static_cast<T>(1.0), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::lerp(static_cast<T>(1.0), static_cast<T>(2.0), static_cast<T>(1.0)), static_cast<T>(2.0), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::lerp(static_cast<T>(1.0), static_cast<T>(2.0), static_cast<T>(0.5)), static_cast<T>(1.5), 1e-4f);
 	BOOST_CHECK_CLOSE(vmath::lerp(static_cast<T>(1.2), static_cast<T>(3.4), static_cast<T>(0.34)), static_cast<T>(1.948), 1e-4f);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
